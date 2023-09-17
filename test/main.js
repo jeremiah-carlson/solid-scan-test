48a4ed320468db349f56676b3a82495aff7c61ef
jeremiah-carlson <carlsonjeremiah2@gmail.com>
2023-09-17 17:03:15 -0400

    Add test input functions

--- test/main.js
+++ test/main.js
@@ -1,19 +1,32 @@
 const helpers = require("./scripts/helpers")
 const puppeteer = require('puppeteer');
 
+const scanners = {
+  concert: {
+    url: "https://salmon-rock-0523f670f.3.azurestaticapps.net/concert",
+    input: "1001-A1-B25",
+  },
+  default: {
+      url: "https://jeremiah-carlson.github.io/solid-scan/#/",
+      input: "1-2",
+    },
+};
+
+
+
 (async () => {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({headless: false});
-  const arr1 = [
-    "https://salmon-rock-0523f670f.3.azurestaticapps.net/concert",
-    "https://salmon-rock-0523f670f.3.azurestaticapps.net/concert", 
-    "https://salmon-rock-0523f670f.3.azurestaticapps.net/concert",
-    "https://salmon-rock-0523f670f.3.azurestaticapps.net/concert",
-    "https://salmon-rock-0523f670f.3.azurestaticapps.net/concert", 
-    "https://salmon-rock-0523f670f.3.azurestaticapps.net/concert",
-  ]
+  const browser = await puppeteer.launch({headless: "new"});
+
+  const arr1 = Array(100).fill(scanners.concert).concat(Array(50).fill(scanners.default));
+
   await arr1.forEach(p=>{
-    helpers.openPage(browser, p, "1001-A1-B25");
+    helpers.testScanner(browser, p.url, p.input, {
+      timeout: {
+        min: 500,
+        max: 6000
+      }
+    });
 
   })
 
--- test/scripts/helpers.js
+++ test/scripts/helpers.js
@@ -2,20 +2,39 @@ const ppt = require('puppeteer');
 
 const helpers = {
 
-    openPage: async function (browser, url, input) {
+    randInt: function(min, max) {
+        return Math.floor((Math.random() * (max-min))+min)
+    },
+
+    enterInput: function (pg, selector, input, options) {
+        pg.waitForSelector(options["selector"]??"#MainInput")
+                .then(_=>
+                    pg.type(options["selector"]??"#MainInput", input, {delay: 200})
+                    )
+                    .then(_=>
+                        pg.keyboard.press("Enter")
+                        .then(_=>
+                            pg.waitForTimeout(this.randInt(options["timeout"]["min"]??100, options["timeout"]["max"]??10000))
+                            .then(_=>
+                                this.enterInput(pg, selector, input, options=options)
+                            )
+                        )
+                    )
+    },
+
+    testScanner: function (browser, url, input, options={}) {
         const selector = "#MainInput";
 
         browser.newPage()
         .then((pg)=>{
-            pg.goto(url)
+            pg.waitForTimeout(this.randInt(0, 60000))
             .then(_=>
-                pg.waitForSelector(selector)
+                pg.goto(url)
                 .then(_=>
-                    pg.type(selector, input, {delay: 100})
-                    )
-                    .then(_=>
-                        pg.keyboard.press("Enter")
-                    )
+                    this.enterInput(pg, selector, input, options={
+                        timeout: 10000
+                    })
+                )  
             )
         })
         
